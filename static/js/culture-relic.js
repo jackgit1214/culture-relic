48135bea3b63ca04af4713e6d245e8870663f7d4
jackgit1214 <hongyullj@sohu.com>
2015-08-15 11:05:36 +0800

    8.15提交

--- static/js/culture-relic.js
+++ static/js/culture-relic.js
@@ -1,44 +1,44 @@
-$(document).ready(function () {
-	
-	
-	
-    docReady();
-    
+$(document).ready(function() {
+	docReady();
 });
 
-
 function docReady() {
-	
-	$("#myCarousel").load("portal/scroll.html",function(){
-		
-	});
-	$("#myModal").on("hidden.bs.modal", function() {
-	    $(this).removeData("bs.modal");
-	});
-	
-	$(".btn-setting").click(function(){
-		var $this = $(this);
-		//$this.addClass("dropdown-toggle");
-		//$this.attr("data-toggle","dropdown");
-		$("#myModal").modal({
-			remote:"login/login.html"
-		});
-		
-	});
-	
-	//
-	//data-toggle="dropdown"
-	$("#signin").click(function(){
-		var $this = $(this);
-		//$this.addClass("dropdown-toggle");
-		//$this.attr("data-toggle","dropdown");
-		$("#myModal").modal({
-			remote:"login/login.html"
-		});
-	});
-	
+
 };
+var relic = {
+	homeRead : function() {
+		$("#myCarousel").load("portal/scroll.html", function() {
 
+		});
+		$("#myModal").on("hidden.bs.modal", function() {
+			$(this).removeData("bs.modal");
+		});
 
+		$(".btn-setting").click(function() {
+			var $this = $(this);
+			$("#myModal").modal({
+				remote : "login/login.html"
+			});
 
+		});
 
+		// data-toggle="dropdown"
+		$("#signin").click(function() {
+			var $this = $(this);
+			// $this.addClass("dropdown-toggle");
+			// $this.attr("data-toggle","dropdown");
+			$("#myModal").modal({
+				remote : "login/login.html",
+				backdrop : "static"
+			});
+		});
+	},
+	manageRead:function(){
+	    $('.navbar-toggle').click(function (e) {
+	        e.preventDefault();
+	        $('.nav-sm').html($('.navbar-collapse').html());
+	        $('.sidebar-nav').toggleClass('active');
+	        $(this).toggleClass('active');
+	    });
+	}
+}
