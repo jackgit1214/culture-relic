97f213b62cf37e3149119dce86ab8f98dafc9f45
jackgit1214 <hongyullj@sohu.com>
2015-08-16 17:23:27 +0800

    菜单组织及内容

--- static/js/culture-relic.js
+++ static/js/culture-relic.js
@@ -1,3 +1,4 @@
+ var msie = navigator.userAgent.match(/msie/i);
 $(document).ready(function() {
 	docReady();
 });
@@ -33,12 +34,36 @@ var relic = {
 			});
 		});
 	},
-	manageRead:function(){
+	manageReady:function(){
 	    $('.navbar-toggle').click(function (e) {
 	        e.preventDefault();
 	        $('.nav-sm').html($('.navbar-collapse').html());
 	        $('.sidebar-nav').toggleClass('active');
 	        $(this).toggleClass('active');
 	    });
+	    
+	    $('ul.main-menu li a').each(function () {
+	        if ($($(this))[0].href == String(window.location))
+	            $(this).parent().addClass('active');
+	    });
+	  
+	    
+	    $('ul.main-menu li a').click(function (e) {
+	        if (msie) e.which = 1;
+	        e.preventDefault();
+	        $('.sidebar-nav').removeClass('active');
+	        $('.navbar-toggle').removeClass('active');
+	        //$('#loading').remove();
+	        //$('#content').fadeOut().parent().append('<div id="loading" class="center">Loading...<div class="center"></div></div>');
+	        
+	        var $clink = $(this);
+	        //History.pushState(null, null, $clink.data('link'));
+	        $('ul.main-menu li.active').removeClass('active');
+	        $("#content").load($clink.data("link"),function(){
+	        	$clink.parent('li').addClass('active');
+	        	//$('#loading').remove();
+	        });
+	    });
+	    
 	}
 }
